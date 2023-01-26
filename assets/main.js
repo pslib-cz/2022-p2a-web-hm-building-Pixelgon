1de10ae91da9b1593c9c70decfd9b6bfae04944e
Pixelgon <pixelgon4@gmail.com>
2023-01-26 11:41:04 +0100

    todo menu

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector('.navbar__hamburger');
+const icons = document.querySelectorAll('.navbar__hamburger-icon');
+const menu = document.querySelector('.navbar__menu');
+
+icons.forEach (icon => {
+    icon.addEventListener('click', (event) => {
+        icon.classList.toggle("open");
+    });
+    hamburger.addEventListener('click', () => {
+        menu.classList.toggle('is-open');
+    })
+});
