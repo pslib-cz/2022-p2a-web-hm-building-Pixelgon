79fefb393ad5cdae17c246a68bb58d444a46b8d7
Pixelgon <pixelgon4@gmail.com>
2023-02-03 13:49:26 +0100

    js fixing...

--- assets/main.js
+++ assets/main.js
@@ -2,7 +2,7 @@ const burger = document.querySelectorAll('.navbar__hamburger');
 const menu = document.querySelector('.navbar__menu');
 
 burger.forEach (icon => {
-    icon.addEventListener('click', () => {
+    icon.addEventListener('click tap', () => {
         icon.classList.toggle("open");
         menu.classList.toggle('open');
     });
