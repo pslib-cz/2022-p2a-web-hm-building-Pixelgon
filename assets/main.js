bbaa1b0477f73dc60c4f6f470a32a11908851109
Pixelgon <pixelgon4@gmail.com>
2023-02-03 13:53:47 +0100

    Stale nic?

--- assets/main.js
+++ assets/main.js
@@ -2,7 +2,23 @@ const burger = document.querySelectorAll('.navbar__hamburger');
 const menu = document.querySelector('.navbar__menu');
 
 burger.forEach (icon => {
-    icon.addEventListener('click tap', () => {
+    icon.addEventListener('click', () => {
+        icon.classList.toggle("open");
+        menu.classList.toggle('open');
+    });
+    icon.addEventListener('touchstart', () => {
+        icon.classList.toggle("open");
+        menu.classList.toggle('open');
+    });
+    icon.addEventListener('touchend', () => {
+        icon.classList.toggle("open");
+        menu.classList.toggle('open');
+    });
+    icon.addEventListener('touchcancel', () => {
+        icon.classList.toggle("open");
+        menu.classList.toggle('open');
+    });
+    icon.addEventListener('touchmove', () => {
         icon.classList.toggle("open");
         menu.classList.toggle('open');
     });
